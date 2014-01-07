84cf497c3e918691063e42e207d448f1e93d5dae
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-07 16:31:54 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Micronews::Application.config.secret_key_base = 'e9f907831d99584f7aaf8e3c8c2708c7243ebf183cd0ca6bda8ad3b2dbe5eec723876ba92c523e8cfe528b837129bd5f33664c5bc880d6de6fd2677ff3aaf2f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Micronews::Application.config.session_store :cookie_store, key: '_micronews_session'
