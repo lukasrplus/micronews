319489d4cbeac8f396731ebfe15b8d6e809ca5fb
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-16 15:21:37 -0600

    Finished User Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
 
   def show
@@ -7,4 +9,22 @@ class UsersController < ApplicationController
     # New Syntax for that
     @user = User.find(params[:id])
   end
+
+   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to MICRONEWS"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
