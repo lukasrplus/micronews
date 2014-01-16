b08540decf6cdf95b4bdfc73039ba9de1f38ed47
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-16 13:32:35 -0600

    Added User Signup Tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    # @user = User.find_by(:id => params[:id])
+    # New Syntax for that
+    @user = User.find(params[:id])
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "Lukas"
+    email "lukas@lukas.com"
+    password "LukasRansi"
+    password_confirmation "LukasRansi"
+  end
+end
+
