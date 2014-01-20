dadb3e75cc0fe6f414fd54239557f353c7614b70
Lukas Ransmayr <lukas.ransmayr@gmail.com>
2014-01-20 17:34:03 -0600

    Finished sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # sign_in is defined as a function
+      # interesting syntax for "user"
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+
+
+
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 
   before_save { email.downcase! }
 
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -10,5 +13,20 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 4 }
 
   has_secure_password
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
 end
